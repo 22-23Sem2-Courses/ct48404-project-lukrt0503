d04a2ee0dffb74e978c696186ec5eba053d1f508
Truong Phan <pvlti.catfish@gmail.com>
2023-04-05 21:47:14 +0700

    Xay dung truong San pham pho bien

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_grocery/component/main_header.dart';
+import 'package:my_grocery/controller/controllers.dart';
 import 'package:my_grocery/view/home/components/popular_category/popular_category.dart';
+import 'package:my_grocery/view/home/components/popular_product/popular_product_loading.dart';
+import 'package:my_grocery/view/home/components/section_title.dart';
 
-import '../../component/main_header.dart';
-import '../../controller/controllers.dart';
-import 'components/carousel_slider/carousel_loading.dart';
 import 'components/carousel_slider/carousel_slider_view.dart';
 import 'components/popular_category/popular_category_loading.dart';
-import 'components/section_title.dart';
+import 'components/carousel_slider/carousel_loading.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,28 +16,44 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Column(children: [
-      Column(
-        children: [
-          const MainHeader(),
-          Obx(() {
-            if (homeController.bannerList.isNotEmpty) {
-              return CarouselSliderView(bannerList: homeController.bannerList);
-            } else {
-              return const CarouselLoading();
-            }
-          }),
-          const SectionTitle(title: "Danh mục phổ biến"),
-          Obx(() {
-            if (homeController.popularCategoryList.isNotEmpty) {
-              return PopularCategory(
-                  categories: homeController.popularCategoryList);
-            } else {
-              return const PopularCategoryLoading();
-            }
-          }),
-        ],
-      )
-    ]));
+        child: Column(
+      children: [
+        const MainHeader(),
+        Expanded(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                Obx(() {
+                  if (homeController.bannerList.isNotEmpty) {
+                    return CarouselSliderView(
+                        bannerList: homeController.bannerList);
+                  } else {
+                    return const CarouselLoading();
+                  }
+                }),
+                const SectionTitle(title: "Danh mục phổ biến"),
+                Obx(() {
+                  if (homeController.popularCategoryList.isNotEmpty) {
+                    return PopularCategory(
+                        categories: homeController.popularCategoryList);
+                  } else {
+                    return const PopularCategoryLoading();
+                  }
+                }),
+                const SectionTitle(title: "Sản phẩm phổ biến"),
+                Obx(() {
+                  if (homeController.popularProductList.isNotEmpty) {
+                    return const PopularProductLoading();
+                  } else {
+                    return const PopularProductLoading();
+                  }
+                }),
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
